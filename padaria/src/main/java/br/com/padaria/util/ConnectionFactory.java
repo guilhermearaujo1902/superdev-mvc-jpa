817c37391639471156b5ac1395b3f177330eb057
Guilherme Araujo <gui.araujo1902@gmail.com>
2024-04-19 20:00:03 -0300

    :construction: feat(app): Configuracao ConnectionFactory

---
+++ padaria/src/main/java/br/com/padaria/util/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.padaria.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionFactory {
+
+	private static EntityManagerFactory factory = null;
+
+	static {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("padaria");
+		}
+	}
+
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
